954787b11dae77c53e1a8ae78b92ec1f0e83e2bf
SabaUrgup <19360859039@ogrenci.btu.edu.tr>
2023-05-18 00:48:57 +0300

    rating feature routes added

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const mongoConfig = require('./config/db')
+const mongoConfig = require('./config/db');
+const ratingRoutes = require('./ratingRoutes'); // ratingRoutes.js dosyasının yolunu doğru şekilde belirtin
 
-
-// mongoose.connect(mongoConfig.connectionString).then(() => {
-//     console.log("Database connected.");
-// }).catch(err => { console.log(err); });
+mongoose.connect(mongoConfig.connectionString).then(() => {
+    console.log("Database connected.");
+}).catch(err => {
+    console.log(err);
+});
 
 app.use(express.json());
 
 const port = 3000;
+
+// Rating rotalarını kullanmak için
+app.use('/api', ratingRoutes);
+
 app.listen(port, () => {
     console.log(`Server is live on port ${port}`);
 });
